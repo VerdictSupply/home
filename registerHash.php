a0d43a928c644280104d7573343099926b721600
Verdict <129124848+VerdictSupply@users.noreply.github.com>
2023-03-27 19:20:44 -0700

    update registration backend to reflect email only instead of username, and initalize mysql db

--- registerHash.php
+++ registerHash.php
@@ -3,32 +3,42 @@ session_start();
 require_once('config.php');
 
 if(isset($_POST['register'])){
-  $username = $_POST['username'];
-  $password = $_POST['password'];
   $email = $_POST['email'];
+  $password = $_POST['password'];
 
   // Validate input
   $errors = array();
-  if(empty($username)) { $errors[] = "Username field is required"; }
+  if(empty($email)) { $errors[] = "email field is required"; }
   if(empty($password)) { $errors[] = "Password field is required"; }
-  if(empty($email)) { $errors[] = "Email field is required"; }
   if(!filter_var($email, FILTER_VALIDATE_EMAIL)) { $errors[] = "Invalid email format"; }
 
-  // Check if username or email already exists
-  $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username OR email = :email");
-  $stmt->execute(['username' => $username, 'email' => $email]);
+  // Create connection to mysql db
+  $servername = "localhost";
+  $username = "verdictsupply";
+  $password = "password";
+  $conn = new mysqli($servername, $username, $password);
+  
+  //output any mysql db connection errors
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+  
+  // Check if email or email already exists
+  $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email OR email = :email");
+  $stmt->execute(['email' => $email, 'email' => $email]);
   $user = $stmt->fetch();
   if($user){
-    if($user['username'] === $username) { $errors[] = "Username already exists"; }
-    if($user['email'] === $email) { $errors[] = "Email already exists"; }
+    if($user['email'] === $email) { $errors[] = "email already exists"; }
   }
 
   // If no errors, hash password and insert user into database
   if(count($errors) === 0){
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (:username, :password, :email)");
-    $stmt->execute(['username' => $username, 'password' => $hashed_password, 'email' => $email]);
+    $stmt = $pdo->prepare("INSERT INTO users (email, password) VALUES (:email, :password)");
+    $stmt->execute(['email' => $email, 'password' => $hashed_password);
     $_SESSION['success'] = "Registration successful!";
+    $conn->close();
     header("Location: login.php");
     return;
   }
